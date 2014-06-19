cd954dea3af630a6598f6a17f21b9097a0dd2628
Thomas Levine <_@thomaslevine.com>
2014-06-19 18:50:05 -0400

    download

---
+++ acpr/download.py
@@ -0,0 +1,27 @@
+@cache(os.path.join(os.path.expanduser('~'), '.acpr'))
+def search(cat:str, pg:int):
+    '''
+    cat is from the input box.
+    pg is a natural number.
+    '''
+    url = 'https://www.regafi.fr/spip.php'
+    params = {
+        'page': 'results',
+        'type': 'advanced',
+        'id_secteur': '3',
+        'lang': 'en',
+        'denomination': '',
+        'siren': '',
+        'cib': '',
+        'bic': '',
+        'nom': '',
+        'siren_agent': '',
+        'num': '',
+        'cat': cat,
+        'retrait': '0',
+        'pg': pg,
+    }
+    return requests.get(url, params = params)
+
+def categories():
+    response = search('not-a-real-category', 1)
